fdb82e38874afa7bfe4e33856e283fff6d8063f3
totalnybet-create <totalnybet@gmail.com>
2026-07-27 11:23:30 +0200

    Podłącz MainActivity do Hilt

--- android-app/app/src/main/java/pl/legnickirynek/app/MainActivity.kt
+++ android-app/app/src/main/java/pl/legnickirynek/app/MainActivity.kt
@@ -3,9 +3,11 @@ package pl.legnickirynek.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import dagger.hilt.android.AndroidEntryPoint
 import pl.legnickirynek.app.ui.LegnickiRynekApp
 import pl.legnickirynek.app.ui.theme.LegnickiRynekTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
