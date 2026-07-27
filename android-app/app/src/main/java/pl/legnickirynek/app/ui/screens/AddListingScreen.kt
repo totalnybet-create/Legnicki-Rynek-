6488a5d7f89760617e7c8730cd1a9b15da1805c3
totalnybet-create <totalnybet@gmail.com>
2026-07-27 10:31:21 +0200

    Dodaj zgodność z eksperymentalnymi komponentami Material 3

--- android-app/app/src/main/java/pl/legnickirynek/app/ui/screens/AddListingScreen.kt
+++ android-app/app/src/main/java/pl/legnickirynek/app/ui/screens/AddListingScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilterChip
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -35,6 +36,7 @@ import pl.legnickirynek.app.ui.theme.LegnicaCoral
 import pl.legnickirynek.app.ui.theme.LegnicaMuted
 import pl.legnickirynek.app.ui.theme.LegnicaNavy
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AddListingScreen(onListingCreated: (Listing) -> Unit) {
     var title by rememberSaveable { mutableStateOf("") }
