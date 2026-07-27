626d6f4e7a115c5a32d20f33d1e59af86df51bdb
totalnybet-create <totalnybet@gmail.com>
2026-07-27 12:54:12 +0200

    Włącz edge-to-edge dla interfejsu Android

--- android-app/app/src/main/java/pl/legnickirynek/app/MainActivity.kt
+++ android-app/app/src/main/java/pl/legnickirynek/app/MainActivity.kt
@@ -3,6 +3,7 @@ package pl.legnickirynek.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import dagger.hilt.android.AndroidEntryPoint
 import pl.legnickirynek.app.ui.LegnickiRynekApp
 import pl.legnickirynek.app.ui.theme.LegnickiRynekTheme
@@ -11,6 +12,7 @@ import pl.legnickirynek.app.ui.theme.LegnickiRynekTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             LegnickiRynekTheme {
                 LegnickiRynekApp()
