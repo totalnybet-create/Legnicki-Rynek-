6e45c394cbe6c1cb263ca786043077727ae79132
totalnybet-create <totalnybet@gmail.com>
2026-07-27 10:23:13 +0200

    Dodaj modele danych aplikacji Android

---
+++ android-app/app/src/main/java/pl/legnickirynek/app/model/Models.kt
@@ -0,0 +1,34 @@
+package pl.legnickirynek.app.model
+
+data class Category(
+    val id: String,
+    val name: String,
+    val symbol: String
+)
+
+data class Listing(
+    val id: String,
+    val title: String,
+    val price: Int,
+    val location: String,
+    val categoryId: String,
+    val description: String,
+    val isFavorite: Boolean = false
+)
+
+data class LocalEvent(
+    val id: String,
+    val title: String,
+    val date: String,
+    val location: String,
+    val description: String
+)
+
+data class Conversation(
+    val id: String,
+    val person: String,
+    val listingTitle: String,
+    val lastMessage: String,
+    val time: String,
+    val unread: Boolean
+)
