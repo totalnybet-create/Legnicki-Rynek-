e1de2107a1c865a115cd31d7fcc7d59420859014
totalnybet-create <totalnybet@gmail.com>
2026-07-28 22:44:52 +0200

    Dokończ naprawę kompilacji Androida

--- android-app/app/src/main/java/pl/legnickirynek/app/ui/screens/ProfileScreen.kt
+++ android-app/app/src/main/java/pl/legnickirynek/app/ui/screens/ProfileScreen.kt
@@ -11,7 +11,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.weight
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
